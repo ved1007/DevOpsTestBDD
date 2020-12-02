c83bbce5a92f5d0ef4fc03aeb425f7abd5d49977
ved100 <ved1007@gmail.com>
2020-12-02 10:59:40 -0600

    Updated the Google Runner file

--- src/main/java/Runner/GoogleRunner.java
+++ src/main/java/Runner/GoogleRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\PC User1\\Desktop\\SeleniumFiles\\RunWithJenins1\\src\\main\\java\\Features\\Google.feature",
+@CucumberOptions(features = "C:\\Users\\PC User1\\Desktop\\SeleniumFiles\\DevOpsTestBDD\\src\\main\\java\\Features\\Google.feature",
 
         glue = {"stepDefinitions" }, 
         format = { "pretty", "html:test-outout", "json:json_output/cucumber.json",
