7c8c1a8e8f4c86c5ad8a4e5e99838ffbd5e07a6c
ved100 <ved1007@gmail.com>
2020-12-05 23:42:04 -0600

    updated DevopsTestBDd project on 5-Dec-2020

--- src/main/java/Runner/GoogleRunner.java
+++ src/main/java/Runner/GoogleRunner.java
@@ -6,13 +6,13 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\PC User1\\Desktop\\SeleniumFiles\\DevOpsTestBDD\\src\\main\\java\\Features\\Google.feature",
+@CucumberOptions(features = "C:\\Users\\PC User1\\Desktop\\SeleniumFiles\\GitMavenNexusTest\\src\\main\\java\\Features\\Gmail.feature",
 
 		glue = { "stepDefinitions" }, plugin = { "pretty", "html:test-outout", "json:json_output/cucumber.json",
 				"junit:junit_xml/cucumber.xml" },
 		monochrome = true,
 		strict = true,
-		dryRun = false)
+		dryRun = true)
 
 public class GoogleRunner {
 
